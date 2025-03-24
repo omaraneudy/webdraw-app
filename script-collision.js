65d28201b0ed78ec11600656c8d3a1786dcb4e33
Omar <omaraneudye@gmail.com>
2025-03-24 12:03:26 -0400

    feat: rectangles can be moved after being drawn

--- script-collision.js
+++ script-collision.js
@@ -27,8 +27,12 @@ let xRectSelect, yRectSelect, widthRectSelect, heightRectSelect;
 
 let rectangles = [];
 
-console.log(dif.x+" x ");
-console.log(dif.y+" y ");
+console.log(dif.x + " x ");
+console.log(dif.y + " y ");
+
+let rectangleId = 0;
+let newPointX = 0;
+let newPointY = 0;
 
 shapeButtons.forEach(shapeButton => {
     shapeButton.addEventListener("click", e => {
@@ -46,11 +50,11 @@ shapeButtons.forEach(shapeButton => {
                 break;
             case "free-hand":
                 selectedShape = "free-hand";
-                break;              
+                break;
             default:
                 break;
         }
-        console.log("desde foreach "+shapeButton.id+" forma seleccionada "+selectedShape);
+        console.log("desde foreach " + shapeButton.id + " forma seleccionada " + selectedShape);
         e.stopPropagation();
 
     })
@@ -64,6 +68,9 @@ let testSelect = false;
 let selectPointX = null;
 let selectPointY = null;
 
+
+let selectedRectangle = null;
+
 document.addEventListener("mousedown", e => {
     switch (selectedShape) {
         case "circle":
@@ -76,18 +83,58 @@ document.addEventListener("mousedown", e => {
             break;
         case "line":
             console.log("we're in line case");
-            selectPointX = e.offsetX;
-            selectPointY = e.offsetY;
-            testSelect = true;
+            xRectSelect = e.offsetX;
+            yRectSelect = e.offsetY;
+            //testSelect = true;
+
+
+            selectedRectangle = rectangles.find(rectangle =>
+            (xRectSelect > rectangle.x && xRectSelect < rectangle.width + rectangle.x
+                && yRectSelect > rectangle.y && yRectSelect < rectangle.y + rectangle.height));
+
+
+            //click on rectangle's border
+            // selectedRectangle = rectangles.find(rectangle =>
+            //     (xRectSelect > rectangle.x && xRectSelect < rectangle.width + rectangle.x 
+            //         && yRectSelect > rectangle.y - 5 && yRectSelect < rectangle.y + 5));
+
+
+            console.log(selectedRectangle);
+            if (!selectedRectangle) return;
+            ctx3.beginPath();
+            ctx3.strokeText(`x=${selectedRectangle.x}, y=${selectedRectangle.y}`, selectedRectangle.x, selectedRectangle.y);
+
+
+            //punto1 =  ,  punto2 = x + width, punto3 = (y + height, x + width), punto4 = y + height 
+
+
+            //Corner1 = (x,y)
+            //Corner2 = x + width
+            //Corner3 = (y + height, x + width)
+            //Corner4 = y + height 
+
+
+            //Corner1 = (x,y)
+            //Corner2 = x + width
+            //Corner3 = (y + height, x + width)
+            //Corner4 = y + height 
+
+
+
+
+
+
+
+
             break;
         case "free-hand":
             break;
         default:
             break;
     }
-            //ctx.lineTo(e.clientX,e.clientY);
+    //ctx.lineTo(e.clientX,e.clientY);
 })
-        
+
 document.addEventListener("mouseup", e => {
     isDrawingLine = false;
     isDrawing = false;
@@ -95,22 +142,48 @@ document.addEventListener("mouseup", e => {
     testSelect = false;
     ctx.closePath();
 
-    if (selectedShape === "square"){
-        rectangles.push(
-        {
-            x: xRect,
-            y: yRect,
-            width: widthRect,
-            height: heightRect
-        });
+    if (selectedShape === "square") {
 
-        xRect = null;
-        yRect = null;
-        widthRect = null;
-        heightRect = null;
+
+        if (xRect && yRect && widthRect && heightRect) {
+            if (rectangles.length == 0) {
+                rectangleId = 0;
+            }
+            else {
+                rectangleId += 1;
+            }
+
+            rectangles.push(
+                {
+                    id: rectangleId,
+                    x: xRect,
+                    y: yRect,
+                    width: widthRect,
+                    height: heightRect
+                });
+
+            xRect = null;
+            yRect = null;
+            widthRect = null;
+            heightRect = null;
+        }
+
 
     }
+    if (selectedRectangle && newPointX && newPointY) {
+        selectedRectangle.x = newPointX;
+        selectedRectangle.y = newPointY;
+        rectangles.push(selectedRectangle);
+        selectedRectangle = undefined;
+        newPointX = undefined;
+        newPointY = undefined;
 
+        // ctx.clearRect(0, 0, 10000, 10000);
+        // rectangles.forEach(rectangle => {
+        //     // ctx.strokeStyle = "black";
+        //     ctx.strokeRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        // });
+    }
 
 
 })
@@ -120,63 +193,78 @@ document.addEventListener("mousemove", e => {
     if (testSquare) {
         xRect = testSquarePointX;
         yRect = testSquarePointY;
-        widthRect = e.offsetX-testSquarePointX;
-        heightRect = e.offsetY-testSquarePointY;
+        widthRect = e.offsetX - testSquarePointX;
+        heightRect = e.offsetY - testSquarePointY;
 
         ctx.clearRect(0, 0, 10000, 10000);
-        
+
         rectangles.forEach(rectangle => {
             ctx.strokeRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
         });
         ctx.strokeRect(xRect, yRect, widthRect, heightRect);
         //ctx.strokeRect(testSquarePointX,testSquarePointY,e.offsetX-testSquarePointX,e.offsetY-testSquarePointY);
-        
+
     }
-    
+
     if (testSelect) {
         xRectSelect = selectPointX;
         yRectSelect = selectPointY;
-        widthRectSelect = e.offsetX-selectPointX;
-        heightRectSelect = e.offsetY-selectPointY;
-        
+        widthRectSelect = e.offsetX - selectPointX;
+        heightRectSelect = e.offsetY - selectPointY;
+
         ctx2.clearRect(0, 0, 10000, 10000);
         // ctx2.clearRect(xRectSelect+1, yRectSelect+1, widthRectSelect+1, heightRectSelect+1);
-        
+
 
 
         rectangles.forEach(rectangle => {
             // ctx.strokeStyle = "black";
             ctx.strokeRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
         });
-        
-        ctx2.strokeStyle = "blue";
-        ctx2.strokeRect(xRectSelect, yRectSelect, widthRectSelect, heightRectSelect);
-        // ctx2.strokeText(`x=${xRectSelect}, y=${yRectSelect}`,xRectSelect, yRectSelect);
-        ctx2.strokeText(`x=${e.offsetX}, y=${e.offsetY}`,e.offsetX, e.offsetY);
-        //console.log(xRectSelect, yRectSelect, widthRectSelect, heightRectSelect);
-        
-        rectangle = rectangles.filter(rectangle => 
-            rectangle.x < xRectSelect + widthRectSelect &&
-            rectangle.x + rectangle.width > xRectSelect &&
-            rectangle.y < yRectSelect + heightRectSelect &&
-            rectangle.y + rectangle.height > yRectSelect
-        )
 
-        //si xrectangulo se encuentra entre xinicial y xfinal
-        
-        
-        if (rectangle.length > 0){
-            ctx3.beginPath();
-            console.log(rectangle);
-            // ctx3.strokeStyle = "red";
-            ctx3.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-            ctx3.strokeText(`x=${rectangle.x}, y=${rectangle.y}`,rectangle.x, rectangle.y);
-            //testSelect = false;
+        ctx2.strokeText(`x=${e.offsetX}, y=${e.offsetY}`, e.offsetX, e.offsetY);
 
 
-        }
+
+    }
+    //xClick=350
+    //x=260
+    //xdifference = 90
+
+
+    if (selectedRectangle) {
+
+        newPointX = e.offsetX - (xRectSelect - selectedRectangle.x);
+        newPointY = e.offsetY - (yRectSelect - selectedRectangle.y);
+
+
+
+        //console.log(e.offsetX-(xRectSelect-selectedRectangle.x),e.offsetY-(yRectSelect-selectedRectangle.y));
+
+
+        rectangles = rectangles.filter(rectangle => rectangle.id !== selectedRectangle.id);
+
+        ctx.clearRect(0, 0, 10000, 10000);
+
+
+
+        ctx.strokeRect(newPointX, newPointY, selectedRectangle.width, selectedRectangle.height);
+
+        // rectangles.forEach(rectangle =>{
+        //     if (rectangle.id === selectedRectangle.id) {
+        //         selectedRectangle.x = newPointX;
+        //         selectedRectangle.y = newPointY;
+        //         return;
+        //     }
+        // })
+
+
+        rectangles.forEach(rectangle => {
+            ctx.strokeRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        });
+
 
     }
 
-    
+
 })
